05e6a18ce6101f037440862c9586b841dec08d0d
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-05 17:35:36 -0400

    Implmenting a test algo

--- sorting/src/App.jsx
+++ sorting/src/App.jsx
@@ -2,33 +2,29 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [graph, setGraph] = useState([
-    {
-      key: 1,
-      value: 200,
-    },
-    {
-      key: 2,
-      value: 400,
-    },
-    {
-      key: 3,
-      value: 100,
-    },
-    {
-      key: 4,
-      value: 800,
-    },
-    {
-      key: 5,
-      value: 300,
-    },
-  ]);
+  const [graph, setGraph] = useState([200, 400, 100, 800, 300]);
+
+  const sort = (graph) => {
+    let newGraph = new Array(5).fill(0)
+    for (let i = 0; i <= 5; i++) {
+      console.log(graph[i])
+      if (graph[i].value > graph[i + 1].value) {
+        let temp = graph[i]
+        graph[i] = graph[i + 1]
+        graph[i + 1] = temp
+      }
+    }
+  }
+
+  const handleClick = () => {
+
+  }
 
   return <div className="App">
+    <button>Start visual</button>
     <div className="container" style={{ display: 'flex', flexFlow: 'row', justifyContent: 'center' }}>
-      {graph.map((item) => (
-        <div style={{ width: 20, height: item.value, backgroundColor: 'purple', border: '1px solid black' }} key={item.key} />
+      {graph.map((item, index) => (
+        <div style={{ width: 20, height: item, backgroundColor: '#87CEFA', border: '1px solid black' }} key={index} />
       ))}
     </div>
 
