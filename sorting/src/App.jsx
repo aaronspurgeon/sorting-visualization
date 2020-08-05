b434a2341f98335959259bf2b21f15dc7dfda346
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-05 18:00:13 -0400

    adding merge sort for better testing

--- sorting/src/App.jsx
+++ sorting/src/App.jsx
@@ -4,24 +4,47 @@ import "./App.css";
 function App() {
   const [graph, setGraph] = useState([200, 400, 100, 800, 300]);
 
-  const sort = (graph) => {
-    let newGraph = new Array(5).fill(0)
-    for (let i = 0; i <= 5; i++) {
-      console.log(graph[i])
-      if (graph[i].value > graph[i + 1].value) {
-        let temp = graph[i]
-        graph[i] = graph[i + 1]
-        graph[i + 1] = temp
+  const merge = (left, right) => {
+    let resultArray = [], leftIndex = 0, rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        console.log(`left index: ${left[leftIndex]}`)
+        resultArray.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        console.log(`right index: ${right[rightIndex]}`)
+        resultArray.push(right[rightIndex]);
+        rightIndex++;
       }
     }
+
+
+    return resultArray
+      .concat(left.slice(leftIndex))
+      .concat(right.slice(rightIndex));
+  }
+
+  const mergeSort = (unsortedArray) => {
+    if (unsortedArray.length <= 1) {
+      return unsortedArray;
+    }
+    const middle = Math.floor(unsortedArray.length / 2);
+
+    const left = unsortedArray.slice(0, middle);
+    const right = unsortedArray.slice(middle);
+
+    return merge(
+      mergeSort(left), mergeSort(right)
+    );
   }
 
   const handleClick = () => {
-
+    mergeSort(graph)
   }
 
   return <div className="App">
-    <button>Start visual</button>
+    <button onClick={handleClick}>Start visual</button>
     <div className="container" style={{ display: 'flex', flexFlow: 'row', justifyContent: 'center' }}>
       {graph.map((item, index) => (
         <div style={{ width: 20, height: item, backgroundColor: '#87CEFA', border: '1px solid black' }} key={index} />
